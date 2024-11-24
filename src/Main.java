4499c5406895bf8c4ed1c8af86475026949e4790
ResistorD <resistord@gmail.com>
2024-11-24 22:28:31 +0400

    Домашка готова.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,133 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Домашка 3.2");
+        System.out.println("Домашка 3.2");
+
+        System.out.println("Задача 1");
+
+        int clientOS;
+
+        clientOS = 0;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+
+        clientOS = 1;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+
+        System.out.println("Задача 2");
+
+        int clientOS1;
+        int clientDeviceYear;
+
+        clientOS1 = 0;
+        clientDeviceYear = 2015;
+
+        if (clientDeviceYear < 2015) {
+            if (clientOS1 == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else if (clientOS1 == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            }
+        } else {
+            if (clientOS1 == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            } else if (clientOS1 == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            }
+
+            clientOS1 = 1;
+            clientDeviceYear = 2012;
+
+            if (clientDeviceYear < 2015) {
+                if (clientOS1 == 0) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+                } else if (clientOS1 == 1) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+                }
+            } else {
+                if (clientOS1 == 0) {
+                    System.out.println("Установите версию приложения для iOS по ссылке.");
+                } else if (clientOS1 == 1) {
+                    System.out.println("Установите версию приложения для Android по ссылке.");
+                }
+            }
+        }
+
+        System.out.println("Задача 3");
+
+        int year = 2021;
+        if (year > 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосным.");
+            } else {
+                System.out.println(year + " год не является високосным.");
+            }
+        }
+
+        System.out.println("Задача 4");
+
+        int deliveryDistance = 95;
+        int deliveryDays;
+
+        if (deliveryDistance <= 20) {
+            deliveryDays = 1;
+        } else if (deliveryDistance <= 60) {
+            deliveryDays = 2;
+        } else if (deliveryDistance <= 100) {
+            deliveryDays = 3;
+        } else {
+            System.out.println("Доставка не доступна для расстояния более 100 км.");
+            return;
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
+
+        System.out.println("Задача 5");
+
+        int monthNumber = 12;
+
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Некорректный номер месяца. Пожалуйста, введите число от 1 до 12.");
+            return;
+        }
+
+        String season = "";
+
+        switch (monthNumber) {
+            case 12: // Декабрь
+            case 1:  // Январь
+            case 2:  // Февраль
+                season = "зима";
+                break;
+            case 3:  // Март
+            case 4:  // Апрель
+            case 5:  // Май
+                season = "весна";
+                break;
+            case 6:  // Июнь
+            case 7:  // Июль
+            case 8:  // Август
+                season = "лето";
+                break;
+            case 9:  // Сентябрь
+            case 10: // Октябрь
+            case 11: // Ноябрь
+                season = "осень";
+                break;
+            default:
+                System.out.println("Нет такого месяца.");
+        }
+
+        System.out.println(monthNumber + "-й месяц принадлежит к сезону " + season + ".");
 
     }
 }
+
